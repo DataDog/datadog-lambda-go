3972bbd785b80d54afaa90fe26268641cf4a98d1
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-06-04 15:01:17 -0400

    Started

--- internal/metrics/kms_decrypter.go
+++ internal/metrics/kms_decrypter.go
@@ -3,16 +3,19 @@
  * under the Apache License Version 2.0.
  *
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
- * Copyright 2019 Datadog, Inc.
+ * Copyright 2021 Datadog, Inc.
  */
 package metrics
 
 import (
 	"encoding/base64"
 	"fmt"
+	"os"
 
+	"github.com/DataDog/datadog-lambda-go/internal/logger"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/service/kms/kmsiface"
 )
 
 type (
@@ -26,6 +29,12 @@ type (
 	}
 )
 
+// functionNameEnvVar is the environment variable that stores the Lambda function name
+const functionNameEnvVar = "AWS_LAMBDA_FUNCTION_NAME"
+
+// encryptionContextKey is the key added to the encryption context by the Lambda console UI
+const encryptionContextKey = "LambdaFunctionName"
+
 // MakeKMSDecrypter creates a new decrypter which uses the AWS KMS service to decrypt variables
 func MakeKMSDecrypter() Decrypter {
 	return &kmsDecrypter{
@@ -33,23 +42,45 @@ func MakeKMSDecrypter() Decrypter {
 	}
 }
 
-func (kd *kmsDecrypter) Decrypt(cipherText string) (string, error) {
+func (kd *kmsDecrypter) Decrypt(ciphertext string) (string, error) {
+	return decryptKMS(kd.kmsClient, ciphertext)
+}
 
-	decodedBytes, err := base64.StdEncoding.DecodeString(cipherText)
+// decryptKMS decodes and deciphers the base64-encoded ciphertext given as a parameter using KMS.
+// For this to work properly, the Lambda function must have the appropriate IAM permissions.
+func decryptKMS(kmsClient kmsiface.KMSAPI, ciphertext string) (string, error) {
+	decodedBytes, err := base64.StdEncoding.DecodeString(ciphertext)
 	if err != nil {
 		return "", fmt.Errorf("Failed to encode cipher text to base64: %v", err)
 	}
 
+	// The Lambda console UI changed the way it encrypts environment variables.
+	// The current behavior as of May 2021 is to encrypt environment variables using the function name as an encryption context.
+	// Previously, the behavior was to encrypt environment variables without an encryption context.
+	// We need to try both, as supplying the incorrect encryption context will cause decryption to fail.
+
+	// Try with encryption context
+	functionName := os.Getenv(functionNameEnvVar)
 	params := &kms.DecryptInput{
 		CiphertextBlob: decodedBytes,
+		EncryptionContext: map[string]*string{
+			encryptionContextKey: &functionName,
+		},
 	}
+	response, err := kmsClient.Decrypt(params)
 
-	response, err := kd.kmsClient.Decrypt(params)
 	if err != nil {
-		return "", fmt.Errorf("Failed to decrypt ciphertext with kms: %v", err)
+		logger.Debug("Failed to decrypt ciphertext with encryption context, retrying without encryption context")
+		// Try without encryption context
+		params = &kms.DecryptInput{
+			CiphertextBlob: decodedBytes,
+		}
+		response, err = kmsClient.Decrypt(params)
+		if err != nil {
+			return "", fmt.Errorf("Failed to decrypt ciphertext with kms: %v", err)
+		}
 	}
-	// Plaintext is a byte array, so convert to string
-	decrypted := string(response.Plaintext[:])
 
-	return decrypted, nil
+	plaintext := string(response.Plaintext)
+	return plaintext, nil
 }
