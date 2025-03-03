11287d33321ef7e39820e54d215ea98c45978882
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-03-03 12:02:55 -0700

    Bump version to 1.21.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,9 +3,9 @@ package version
 
 // DDLambdaVersion is the current version number of this library (datadog-lambda-go).
 // It is automatically updated by the release script.
-const DDLambdaVersion = "1.20.0"
+const DDLambdaVersion = "1.21.0"
 
 // DDTraceVersion is the version of dd-trace-go required by this libary.
 // This should match the version number specified in go.mod.
 // It is automatically updated by the release script
-const DDTraceVersion = "1.65.1"
+const DDTraceVersion = "1.72.1"
